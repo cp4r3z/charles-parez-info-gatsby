301ceaa957e929a6109670f878febbdaf1aa6451
cp4r3z <cp4r3z@gmail.com>
2021-07-22 19:11:20 -0500

    gatsby-plugin-manifest for favicon and PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://charles.parez.info",
     title: "Charles Parez",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      // https://github.com/gatsbyjs/gatsby/tree/master/packages/gatsby-plugin-manifest
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/images/favicon/favicon-source-cjp-head-1500.png`,
+        // name: `GatsbyJS`,
+        // short_name: `GatsbyJS`,
+        // start_url: `/`,
+        // background_color: `#f7f0eb`,
+        // theme_color: `#a2466c`,
+        // display: `standalone`,
+      },
+    },
+  ],
 };
